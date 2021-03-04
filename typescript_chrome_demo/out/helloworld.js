9176cc6c3e4ee6a973df37e89372ca203bc0876c
Potter <75838259@qq.com>
2021-03-04 22:46:30 +0800

    submit test project

---
+++ typescript_chrome_demo/out/helloworld.js
@@ -0,0 +1,4 @@
+"use strict";
+var message = 'Hello Web, I am is TypeSciprt';
+console.log(message);
+document.body.textContent = message;
---
+++ typescript_chrome_demo/src/helloworld.ts
@@ -0,0 +1,3 @@
+let message: string = 'Hello Web, I am is TypeSciprt';
+console.log(message);
+document.body.textContent = message;
