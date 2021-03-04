d931aa68efe7f7ab4b1be22175c046711eca7448
Potter <75838259@qq.com>
2021-03-04 23:53:17 +0800

    学习到Interface

--- typescript_node_demo/out/test.js
+++ typescript_node_demo/out/test.js
@@ -1,3 +1,17 @@
 var message = 'Hello Web, I am is TypeSciprt abc';
 console.log(message);
+var Color;
+(function (Color) {
+    Color[Color["Red"] = 0] = "Red";
+    Color[Color["Green"] = 10] = "Green";
+    Color[Color["Blue"] = 11] = "Blue";
+})(Color || (Color = {}));
+var c = Color.Green;
+console.log(Color[c]);
+function printLabel(labelledObj) {
+    console.log(labelledObj.label);
+    console.log(labelledObj);
+}
+var myObj = { size: 10, label: "Size 10 Object" };
+printLabel(myObj);
 //# sourceMappingURL=test.js.map
--- typescript_node_demo/src/test.ts
+++ typescript_node_demo/src/test.ts
@@ -1,10 +1,14 @@
-/**
-* 创建日期: 2021-03-04
-* 文件名称：test.ts
-* 创建作者：Potter
-* 开发版本：1.0.0
-* 相关说明：
-*/
 let message: string='Hello Web, I am is TypeSciprt abc';
-
 console.log(message);
+
+enum Color {Red, Green=10, Blue}
+let c: Color = Color.Green;
+console.log(Color[c]);
+
+function printLabel(labelledObj: { label: string }) {
+    console.log(labelledObj.label);
+    console.log(labelledObj);
+  }
+  
+  let myObj = { size: 10, label: "Size 10 Object" };
+  printLabel(myObj);
